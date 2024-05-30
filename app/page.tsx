87ca58bd16ce604cc3a1908c72978fc0542d866a
Pavel Sušický <me@frontendista.cz>
2024-05-30 10:18:34 +0200

    chore(i18n): add optimization plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Button } from "@/aria/Button";
+import { useDateFormatter } from "react-aria";
 
 export default function Home() {
+	let formatter = useDateFormatter();
+
 	return (
 		<main className="flex items-center justify-center h-screen">
+			<p>{formatter.format(new Date())}</p>
 			<Button
 				onPress={() => {
 					console.log("XD");
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import optimizeLocales from "@react-aria/optimize-locales-plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		config.plugins.push(
+			optimizeLocales.webpack({
+				locales: ["en-US", "cs-CZ"],
+			})
+		);
+		return config;
+	},
+};
 
 export default nextConfig;
